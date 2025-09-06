7d870e4f824ef8c923562b95db2e8da9ce478c18
max-durnea <maxim.durnea@mail.ru>
2025-09-06 19:55:00 +0300

    Add basic command handling

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,7 +5,7 @@ import(
 	"encoding/json"
 )
 
-
+//A special struct to store the config file data which allows us to edit the file easily
 type Config struct{
 	DB_url string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
@@ -15,21 +15,22 @@ const configFileName = ".gatorconfig.json"
 
 
 func Read() (Config,error){
-
+	//find the home directory because the config file is stored by default inside the home directory
 	homedir,err := os.UserHomeDir()
 	if err != nil{
 		return Config{}, err
 	}
-
+	//build the path to the file
 	file:=homedir+"/"+configFileName
 	if err != nil{
 		return Config{}, err
 	}
-
+	//read file contents as a string
 	contents,err := os.ReadFile(file)
 	if err != nil {
 		return Config{}, err
 	}
+	//unmarshal the contents into a Config struct
 	var cfg Config
 	err = json.Unmarshal(contents,&cfg)
 	if err != nil {
@@ -39,13 +40,16 @@ func Read() (Config,error){
 }
 
 func (cfg Config)SetUser(username string) error{
+	//edit the config struct with the specified username
 	cfg.CurrentUserName=username
 	homedir,err:=os.UserHomeDir()
 	if err != nil {
 		return err
 	}
 	file:= homedir+"/"+configFileName
+	//build the byte slice of the Config struct
 	js,err:= json.Marshal(cfg)
+	//write the slice to the file 0666 is used to allow any user to read and write to the file but not execute it
 	if err := os.WriteFile(file, js,0666); err != nil {
 		return err
 	}
--- main.go
+++ main.go
@@ -1,24 +1,78 @@
 package main
 import (
 	"fmt"
-	//"os"
+	"os"
 	"github.com/max-durnea/blog-aggregator/internal/config"
 )
+//maintain the state, here we have the Config struct which is built by reading the config file
 type state struct{
-	cfg Config*
+	cfg *config.Config
 }
 
 type command struct{
-	
+	name string
+	args []string
+}
+//maintain the commands in a map of name->function
+type commands struct{
+	handlers map[string]func(*state, command) error
+}
+//run a command
+func (c *commands) run(s *state, cmd command){
+	handler,ok := c.handlers[cmd.name]
+	if !ok {
+		fmt.Printf("ERROR: The provided command does not exist\n")
+		return
+	}
+	err:=handler(s,cmd)
+	if err != nil{
+		fmt.Printf("%v\n",err)
+		os.Exit(1)
+	}
+}
+//register a new command
+func (c *commands) register(name string, f func(*state, command) error){
+	c.handlers[name]=f
 }
 func main(){
-	
+	//initialize config, state, commands structs to use them later
+	var cfg config.Config
+	st := state{}
+	st.cfg = &cfg
+	cmds := commands{
+    	handlers: map[string]func(*state, command) error{},
+	}
+	//read the config file into the Config struct
 	cfg,err:=config.Read()
 	if err!=nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
-	err=cfg.SetUser("yagan")
-	cfg,err = config.Read()
-	fmt.Printf("%v\n",cfg)
+	
+	//Here we add new commands
+	cmds.register("login",handlerLogin)
+
+	//Get the command line arguments
+	args:=os.Args
+	if(len(args)<2){
+		fmt.Println("ERROR: No arguments provided!")
+		os.Exit(1)
+	}
+	//Execute the command
+	cmd := command{name : args[1], args : args[2:]}
+	cmds.run(&st,cmd)
+	
+}
+
+func handlerLogin(s *state, cmd command) error{
+	if len(cmd.args)==0 {
+		return fmt.Errorf("ERROR: Username not provided")
+	}
+	//Write to the config file the new Username
+	err:=s.cfg.SetUser(cmd.args[0])
+	if err != nil {
+		return err
+	}
+	fmt.Println("User has been set successfuly!")
+	return nil
 }
