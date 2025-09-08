2f83316365f1c4ae7a9c5013e70aa60a2eda974c
max-durnea <maxim.durnea@mail.ru>
2025-09-08 12:51:15 +0300

    Add script to clear database easily

---
+++ clear_database.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+cd sql/schema
+goose postgres "postgres://postgres:postgres@localhost:5432/gator" down
+goose postgres "postgres://postgres:postgres@localhost:5432/gator" up
+cd -
